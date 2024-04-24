f19c0bf239944c644df134374f721b830068ba5b
Rishadey <139986138+Rishadey@users.noreply.github.com>
2024-04-24 15:04:25 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,33 @@
+let songs =  [
+    {
+        name: 'Cruel Summer',
+        path: 'musics/Taylor-Swift-Cruel.mp3',
+        artist: 'Taylor Swift',
+        cover: 'images/cruel summer.jpg'
+    },
+    {
+        name: 'I don\'t wanna live forever ',
+        path: 'musics/I-Dont-Wanna-Live-Foreve.mp3',
+        artist: 'ZAYN & Taylor Swift',
+        cover: 'images/I dont wanna live.jpg'
+    },
+    {
+        name: 'Closer',
+        path: 'musics/Closer.mp3',
+        artist: 'The Chainsmokers ft. Halsey ',
+        cover: 'images/closer.png'
+    },
+    {
+        name: 'Back to December',
+        path: 'musics/Back-To-December.mp3',
+        artist: 'Taylor Swift',
+        cover: 'images/Back_to_December.png'
+    },
+    {
+        name: 'Steal my girl',
+        path: 'musics/Steal-My-Girl.mp3',
+        artist: 'One Direction',
+        cover: 'images/Steal_My_Girl.png'
+    },
+
+]
